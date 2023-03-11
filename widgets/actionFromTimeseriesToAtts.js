1adf438680c33e15be9074397a401fc5c6a584a8
andresropk <afrokap@gmail.com>
2023-03-11 08:30:53 -0500

    feat: beta- action row retreives and log telemetry

--- widgets/actionFromTimeseriesToAtts.js
+++ widgets/actionFromTimeseriesToAtts.js
@@ -1,3 +1,4 @@
+let log = console.log
 
 //  injector is like an API getter
 let $injector = widgetContext.$scope.$injector;
@@ -6,16 +7,37 @@ let attributeService = $injector.get(widgetContext
     .servicesMap.get('attributeService'));
 
 console.log(additionalParams)
-// build the msg array
+// coger el ts_id
+let ts_id = additionalParams['0']
+
+// build the msg array de sta, std, blockin, pushback, isClosed, +20
+let keys = ['regNum','sta','std','blockin','pushback']
+
+// get la telemetría de ese ts_id -> 
+attributeService.getEntityTimeseries(entityId,keys,ts_id-2,ts_id+2).subscribe(
+        function(telemetry){
+            log('telemetry\n',telemetry);
+            let attributesArray = []
+            for (let key in telemetry){
+                attributesArray.push({key:key,value:telemetry[key][0].value})
+            }
+            log('attributesArray\n',attributesArray);
+        }
+    )
+
 let attributesArray = [{key:'regNum',value:additionalParams["1"]}];
 
+// updateAttributes()
+
 // Update esa mondá
-attributeService.saveEntityAttributes(entityId, 'SHARED_SCOPE', attributesArray).subscribe(
+function updateAttributes(){
+    attributeService.saveEntityAttributes(entityId, 'SHARED_SCOPE', attributesArray).subscribe(
                     function () {
                         widgetContext.updateAliases();
                         openDashboardState('editar_operacion');
                     }
                 );
+}
 
 // function to do what it says
 function openDashboardState(stateId) {
