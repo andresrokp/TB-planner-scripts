1d53f9dd846b03f49705ab300b032288af03c560
andresropk <afrokap@gmail.com>
2023-03-21 17:26:16 -0500

    chore: ts_id on click - vis data GET

--- widgets/widgetTimelineSeriesV3.js
+++ widgets/widgetTimelineSeriesV3.js
@@ -7,7 +7,7 @@ self.onInit = async function() {
     let startTime = performance.now();
     
     // ctx.data y ctx.datasource are the main data carriers
-    console.log('ctx\n', self.ctx);
+    // console.log('ctx\n', self.ctx);
     // log('ctx.datasources\n', self.ctx.datasources);
     // log('ctx.data\n',self.ctx.data);
     
@@ -31,7 +31,7 @@ self.onInit = async function() {
     // guard to avoid development joda to save
     if (!data) data = []
 
-    log('my data\n',data);
+    // log('my data\n',data);
     // iterate over [.data] to assembly la verga with the desired info
     data.forEach(e => {
         // log('e\n',e);
@@ -52,10 +52,11 @@ self.onInit = async function() {
     for(let device in mainStore){
         // log('device\n',device)
         // log("mainStore[device]['regNum']",mainStore[device]['regNum'])
-        mainStore[device]['regNum'].forEach((e,i)=>{
+        let deviceData = mainStore[device]
+        deviceData['regNum'].forEach((e,i)=>{
             // log('items e,i\n',`${e},${i}`)
-            var start = new Date(mainStore[device]['sta'][i]);
-            var end = new Date(mainStore[device]['std'][i]);
+            var start = new Date(deviceData['sta'][i]);
+            var end = new Date(deviceData['std'][i]);
             let diffMillis = end - start;
             let diffHours = Math.round((diffMillis / 1000 / 60 / 60) * 10) / 10;
             items.add({
@@ -63,10 +64,12 @@ self.onInit = async function() {
                 group: device,
                 start: start,
                 end: end,
-                content1: mainStore[device]['regNum'][i],
-                content2: diffHours,
-                content: `${mainStore[device]['regNum'][i]}::${diffHours}hr`
+                regNum: deviceData['regNum'][i],
+                ts_id: deviceData['ts_id'][i],
+                diffHours: diffHours,
+                content: `${deviceData['regNum'][i]}::${diffHours}hr`
             });
+            // console.log(e,i,items)
         })
     }
 
@@ -99,10 +102,10 @@ self.onInit = async function() {
         tooltip: {
             template: function(itemData,parsedItemData){
                 return `<span><strong>Details</strong></span><br>
-                        <span>reg: ${itemData.content1}</span><br>
+                        <span>reg: ${itemData.regNum}</span><br>
                         <span>in: ${timeRemaining(itemData.start)}</span><br>
                         <span>at: ${formatDateAndTime(itemData.start)}</span><br>
-                        <span>lasts: ${itemData.content2}hr</span>`
+                        <span>lasts: ${itemData.diffHours}hr</span>`
             }
         },
         margin: {
@@ -118,11 +121,19 @@ self.onInit = async function() {
     timeline.setGroups(groups);
     timeline.setItems(items);
     
-    if (self.ctx.dashboardWidget.widgetId == "05cf4465-5da7-4450-8654-c838c50a9ef2") console.log('hola widget pruebas')
-    timeline.on('doubleClick', function(props){
-        console.log('ctx\n',self.ctx.stateController);
-        log('props\n',props)
-    })
+    if (self.ctx.stateController.dashboardId == "c7c456b0-b450-11ed-9b83-e14509358390" && self.ctx.dashboard.authUser.tenantId == "87191010-8d9d-11ea-8896-2dbeb466d642"){
+        console.log('hola dashboard pruebas')
+        console.log('ctx\n', self.ctx);
+        log('my data\n',data);
+        log('items\n',items);
+        console.log('items async\n',items);
+        timeline.on('doubleClick', function(event){
+            console.log('ctx.stateController\n',self.ctx.stateController);
+            log('props\n',event)
+            log('item\n',event.item)
+            log('item ts_id\n',items.get(event.item).ts_id)
+        })
+    }
 }
 
 self.onDataUpdated = function() {
