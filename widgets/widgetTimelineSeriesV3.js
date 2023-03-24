ee8d7f82acc46c5c943024c1288ee3d8542a6c51
andresropk <afrokap@gmail.com>
2023-03-24 16:43:30 -0500

    refactor: TLv3 more readable
    -Use of deconstructing
    -Object.entries() to extract variables
    -Variables Rename
    -Better comments

--- widgets/widgetTimelineSeriesV3.js
+++ widgets/widgetTimelineSeriesV3.js
@@ -1,20 +1,18 @@
 
-let log = function(msg, obj) {
-    console.log(msg, JSON.parse(JSON.stringify(obj)));
-}
+let log = (msg, obj) => console.log(msg, JSON.parse(JSON.stringify(obj)));
+
+let wait = async (ms) => await new Promise(resolve => setTimeout(resolve, ms));
 
 self.onInit = async function() {
-    let startTime = performance.now();
-    
     // ctx.data y ctx.datasource are the main data carriers
     console.log('ctx\n', self.ctx);
     // log('ctx.datasources\n', self.ctx.datasources);
     // log('ctx.data\n',self.ctx.data);
     
     // corroncho mechanism to wait for data array full load
-    await new Promise(resolve => setTimeout(resolve, 100));
-    data = [...(self.ctx.data)]
-    await new Promise(resolve => setTimeout(resolve, 100));
+    await wait(100)
+    dataCopy = [...(self.ctx.data)] || []
+    await wait(100)
 
     // hash to store the devices names as visJS needs; with id and content
     let turnarounds = self.ctx.datasources.map((e,i) => {
@@ -25,84 +23,83 @@ self.onInit = async function() {
     });
     // log('turnarounds\n',turnarounds);
 
-    // empty hash to organice the info. It is a Hash of Hashes of arrays. The main key are for device name, the second hash is for the key values {sta:[],std:[],rN:[]}. Inside will be the timeseries values of each
+    // Empty hash to organice the info.
+    // It is a Hash of Hashes of arrays.
+    // The main key are for device name
+    // the second hash is for the key values {sta:[],std:[],rN:[]}.
+    // Inside will be the timeseries values of each key
     let mainStore = {};
     
-    // guard to avoid development joda to save
-    if (!data) data = []
-
-    // log('my data\n',data);
-    // iterate over [.data] to assembly la verga with the desired info
-    data.forEach(e => {
+    // iterate over [.dataCopy] to assembly la verga with the desired info
+    dataCopy.forEach(e => {
         // log('e\n',e);
-        let entityName = e.datasource.entityName;
-        let dataKey = e.dataKey.name;
+        let { entityName, entityId } = e.datasource;
+        let { name : dataKey } = e.dataKey;
         let dataArray = e.data.map(d => d[1]);
 
         if (!mainStore[entityName]) mainStore[entityName] = {};
         mainStore[entityName][dataKey] = dataArray;
-        mainStore[entityName]['entityId'] = self.ctx.data[0].datasource.entityId;
-    })
+        mainStore[entityName].entityId = entityId;
+    });
     // log('mainStore',mainStore);
 
-    // create items
-    var groups = new vis.DataSet(turnarounds);
-    var items = new vis.DataSet();
-    let j = 1;
+    // create visJS objects
+    const groups = new vis.DataSet(turnarounds);
+    const items = new vis.DataSet();
     
-    let actualExpectedTest = false;
-    if (self.ctx.dashboard.authUser.tenantId == "87191010-8d9d-11ea-8896-2dbeb466d642"
-        && self.ctx.stateController.dashboardId == "3cb087c0-c859-11ed-9b83-e14509358390"){
-            actualExpectedTest = true;                 
-    }
-
-    for(let device in mainStore){
-        // log('device\n',device)
-        // log("mainStore[device]['regNum']",mainStore[device]['regNum'])
-        let deviceData = mainStore[device]
-        deviceData['regNum'].forEach((e,i)=>{
-            // log('items e,i\n',`${e},${i}`)
-            var start = new Date(deviceData['sta'][i]);
-            var end = new Date(deviceData['std'][i]);
+    const actualExpectedTest = (self.ctx.dashboard.authUser.userId == "38f40310-2c68-11ed-9e8e-83451eb09aa6"
+                            && self.ctx.dashboard.authUser.firstName == "Andres")
+                            // && self.ctx.stateController.dashboardId == "3cb087c0-c859-11ed-9b83-e14509358390")
+    
+    let j = 1;
+    // extract the info from mainStore and work with it as is
+    for(let [device,deviceData] of Object.entries(mainStore)){
+        
+        // extract relevant data from the device
+        const { regNum,sta,std,ts_id,entityId,blockin,pushback } = deviceData
+        for (let i=0; i<regNum.length; i++){
+            
+            var start = new Date(sta[i]);
+            var end = new Date(std[i]);
             let diffMillis = end - start;
             let diffHours = Math.round((diffMillis / 1000 / 60 / 60) * 10) / 10;
+            
             items.add({
                 id: ++j,
                 group: device,
-                deviceId : deviceData['entityId'],
-                start: start,
-                end: end,
-                regNum: deviceData['regNum'][i],
-                ts_id: deviceData['ts_id'][i],
-                diffHours: diffHours,
-                content: `${deviceData['regNum'][i]}::${diffHours}hr`
+                entityId,
+                start,
+                end,
+                regNum: regNum[i],
+                ts_id: ts_id[i],
+                diffHours,
+                content: `${regNum[i]}::${diffHours}hr`
             });
             
-            if (actualExpectedTest && deviceData['blockin'][i] && deviceData['pushback'][i]){
-                console.log('Hello only actual-current test')
-                let operationStart = new Date(deviceData['blockin'][i]);
-                let operationEnd = new Date(deviceData['pushback'][i]);
+            if (actualExpectedTest && blockin[i] && pushback[i]){
+                let operationStart = new Date(blockin[i]);
+                let operationEnd = new Date(pushback[i]);
                 let operationDiffMillis = operationEnd - operationStart;
                 let operationDiffMinutes = Math.round((operationDiffMillis / 1000 / 60 ) * 10) / 10;
                 items.add({
                     id: ++j,
                     group: device,
                     className: 'operation',
-                    deviceId : deviceData['entityId'],
+                    entityId,
                     start: operationStart,
                     end: operationEnd,
-                    regNum: deviceData['regNum'][i],
-                    ts_id: deviceData['ts_id'][i],
+                    regNum: regNum[i],
+                    ts_id: ts_id[i],
                     diffHours: operationDiffMinutes,
                     content: `${operationDiffMinutes}min`
                 });
             }
-        });
+        }
     }
 
     function formatDateAndTime(aDate) {
         let daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        let date = aDate.getDate().toString().padStart(2, '0');
+        let date = aDate.getDate().toString().padStart(2, '0'); // rellena el 0
         let dayOfWeek = daysOfWeek[aDate.getDay()];
         let hours = aDate.getHours().toString().padStart(2, '0');
         let minutes = aDate.getMinutes().toString().padStart(2, '0');
@@ -111,32 +108,30 @@ self.onInit = async function() {
     
     function timeRemaining(futureTime) {
         let now = new Date();
-        let timeRemaining = Math.max(0, futureTime - now); // ensure not get negative values
+        let timeRemaining = Math.max(0, futureTime - now); // to not take negatives
         let hoursRemaining = Math.floor(timeRemaining / (60 * 60 * 1000)); // get hours in whole number
-        let minutesRemaining = Math.floor((timeRemaining / (60 * 1000)) % 60); // get minutes, then the reminder quantity
+        let minutesRemaining = Math.floor((timeRemaining / (60 * 1000)) % 60); // get reminder in minutes
         return `${hoursRemaining} hrs, ${minutesRemaining} min`;
     }
     
-    
-
-    // specify options
+    // specify visJS options
     var options = {
         stack: true,
         start: new Date(),
-        end: new Date(1000 * 60 * 60 * 48 + (new Date()).valueOf()),
+        end: new Date(1000 * 60 * 60 * 24 + (new Date()).valueOf()), // one day span
         selectable: false,
         editable: false,
         tooltip: {
             template: function(itemData,parsedItemData){
-                return `<span><strong>Details</strong></span><br>
+                return `<b><span>Details</span></b><br>
                         <span>reg: ${itemData.regNum}</span><br>
                         <span>in: ${timeRemaining(itemData.start)}</span><br>
                         <span>at: ${formatDateAndTime(itemData.start)}</span><br>
-                        <span>lasts: ${itemData.diffHours}hr</span>`;
+                        <span>lasts: ${itemData.diffHours}hr</span>`
             }
         },
         margin: {
-            item: 10, // between items
+            item: 3, // between items
             axis: 5 // between items and the axis
         },
         orientation: 'top'
@@ -148,32 +143,23 @@ self.onInit = async function() {
     timeline.setGroups(groups);
     timeline.setItems(items);
     
-    
-    if ( self.ctx.dashboard.authUser.tenantId == "87191010-8d9d-11ea-8896-2dbeb466d642"
-            && (self.ctx.stateController.dashboardId == "c7c456b0-b450-11ed-9b83-e14509358390") ){
-        console.log('hola on click test');
-        console.log('self\n', self);
-        console.log('ctx\n', self.ctx);
-        log('my data\n',data);
-        log('items\n',items);
-        console.log('items async\n',items);
+    if ( self.ctx.dashboard.authUser.userId == "38f40310-2c68-11ed-9e8e-83451eb09aa6"
+    && self.ctx.dashboard.authUser.firstName == "Andres"
+    && self.ctx.stateController.dashboardId == "c7c456b0-b450-11ed-9b83-e14509358390"){
         timeline.on('doubleClick', function(event){
-            console.log('ctx.stateController\n',self.ctx.stateController);
-            log('props\n',event);
-            log('service item\n',event.item);
-            let item = items.get(event.item);
-            log('real item\n',item);
-            log('item ts_id\n',item.ts_id);
-            telemetryLoadPushAndGo('selected_item',item.deviceId,item.group,item.ts_id );
+            log('event\n',event)
+            log('items.get(event.item)\n',items.get(event.item))
+            const { entityId,group,ts_id } = items.get(event.item)
+            telemetryLoadPushAndGo('selected_item',entityId,group,ts_id )
         })
     }
     
-    // function similar to the custom action in the timeseries table of operations in the planner second dashboard state editar_operación
-    function telemetryLoadPushAndGo(stateId, deviceId, deviceName, ts_id){
+    // BIG function similar to the custom action in the timeseries table of operations in the planner second dashboard state editar_operación
+    function telemetryLoadPushAndGo(stateId, _entityId, entityName, ts_id){
         // here we need to put-in this parameters values "manually"
         let entityId = {
             "entityType": "DEVICE",
-            "id": deviceId
+            "id": _entityId
         }
         let $injector = self.ctx.$scope.$injector;
         let attributeService = $injector.get(self.ctx.servicesMap.get('attributeService'));
@@ -202,10 +188,7 @@ self.onInit = async function() {
         }
         
         function openDashboardState() {
-            let params = {
-                entityId: entityId,
-                entityName: deviceName
-            }
+            let params = { entityId, entityName }
             self.ctx.stateController.openState(stateId,params,false);
         }
     }
