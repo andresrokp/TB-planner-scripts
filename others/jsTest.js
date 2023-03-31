477fca977897a25de9b3cd8509792710c273c3c3
andresropk <afrokap@gmail.com>
2023-03-31 18:20:47 -0500

    chore: init script to register experiments

---
+++ others/jsTest.js
@@ -0,0 +1,13 @@
+// remote script loading
+
+function usingEmbededScriptTag(){
+    let ans = ''
+    let scriptElement = document.createElement('script')
+    scriptElement.src = 'https://github.com/andresrokp/TB-planner-scripts/blob/master/widgets/fechaBacanaAttCard.js'
+    scriptElement.onload = function(){
+        ans = eval('fechaBacana')
+    }
+    document.head.appendChild(scriptElement);
+    return ans;
+}
+
