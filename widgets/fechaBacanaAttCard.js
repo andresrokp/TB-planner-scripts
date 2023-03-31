d6ddf1d5296c5c01c4b20a3b48ae5e0a7babffd6
andresropk <afrokap@gmail.com>
2023-03-31 12:39:11 -0500

    fix: wrap script in a function

--- widgets/fechaBacanaAttCard.js
+++ widgets/fechaBacanaAttCard.js
@@ -1,13 +1,15 @@
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dic"];
-var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+function fechaBacana(value){
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dic"];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-let date = new Date(value);
-let year = date.getFullYear();
-let month = monthNames[date.getMonth()];
-let day = date.getDate();
-let dayName = days[date.getDay()];
-let hour = date.getHours();
-let minutes = date.getMinutes();
-minutes = minutes <= 9 ? `0${minutes}`: minutes;
+    let date = new Date(value);
+    let year = date.getFullYear();
+    let month = monthNames[date.getMonth()];
+    let day = date.getDate();
+    let dayName = days[date.getDay()];
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    minutes = minutes <= 9 ? `0${minutes}`: minutes;
 
-return `${dayName} ${day} ${month} ${hour}:${minutes}`;
+    return `${dayName} ${day} ${month} ${hour}:${minutes}`;
+}
