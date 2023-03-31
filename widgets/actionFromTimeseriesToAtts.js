e055ee829a443f36f7327959cf29501c4f0960f9
andresropk <afrokap@gmail.com>
2023-03-31 18:22:10 -0500

    feat: onRowClick delete not present attributes

--- widgets/actionFromTimeseriesToAtts.js
+++ widgets/actionFromTimeseriesToAtts.js
@@ -1,4 +1,6 @@
 let log = console.log
+log('entityId\n',entityId)
+log('entityName\n',entityName)
 
 //  injector is like an API getter
 let $injector = widgetContext.$scope.$injector;
@@ -16,34 +18,39 @@ let keys = ['ts_id','regNum','sta','std','blockin','pushback','echo','TT1','TT2'
 // get la telemetría de ese ts_id -> 
 attributeService.getEntityTimeseries(entityId,keys,ts_id-2,ts_id+2).subscribe(
         function(telemetry){
-            log('telemetry\n',telemetry);
-            let attributesArray = []
-            for (let key in telemetry){
-                attributesArray.push({key:key,value:telemetry[key][0].value})
+            
+            let attributesData = []
+            let attsToDelete = []
+            
+            for (let key of keys){
+                if(telemetry[key]) attributesData.push({key:key,value:telemetry[key][0].value})
+                else attsToDelete.push({key:key});
             }
-            log('attributesArray\n',attributesArray);
-            updateAttributes(attributesArray)
+            
+            updateAttributes(attributesData,attsToDelete)
         }
     )
 
 
 // Update esa mondá
-function updateAttributes(attributesArray){
-    attributeService.saveEntityAttributes(entityId, 'SHARED_SCOPE', attributesArray).subscribe(
-                    function () {
-                        widgetContext.updateAliases();
-                        openDashboardState('editar_operacion');
-                    }
-                );
+function updateAttributes(attributesData,attsToDelete){
+    attributeService.saveEntityAttributes(entityId, 'SHARED_SCOPE', attributesData) //save atts
+    .subscribe(
+        function () {
+            console.log(attsToDelete)
+            attributeService.deleteEntityAttributes(entityId, 'SHARED_SCOPE', attsToDelete) //delete atts
+            .subscribe(
+                function () {
+                    openDashboardState('editar_operacion');
+                    widgetContext.updateAliases();
+                }
+            )
+        }
+    );
 }
 
 // function to do what it says
 function openDashboardState(stateId) {
-    let params = {
-        entityId: entityId,
-        entityName: entityName
-    }
-
-    widgetContext.stateController.openState(stateId, params,
-        false);
+    let params = { entityId, entityName }
+    widgetContext.stateController.openState(stateId, params, false);
 }
