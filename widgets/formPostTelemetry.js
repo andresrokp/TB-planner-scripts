f416edd813532a2a0437812d14eb48b7d08173fb
andresropk <afrokap@gmail.com>
2023-03-30 21:21:04 -0500

    fix: race condition prevented when saving teltry

--- widgets/formPostTelemetry.js
+++ widgets/formPostTelemetry.js
@@ -1,8 +1,7 @@
-// Confirmation logic
+// Confirmation alert
 let choose = confirm('Are you sure to save the data?');
 if (!choose) return;
 
-// 
 let $injector = widgetContext.$scope.$injector;
 let attributeService = $injector.get(widgetContext.servicesMap.get('attributeService'));
 
@@ -11,22 +10,17 @@ let keys = ['regNum','sta','std'];
 
 // get SHARED attributes -> 
 attributeService.getEntityAttributes(entityId, 'SHARED_SCOPE', keys).subscribe(
-        async function(atts){
-            await new Promise(resolve => setTimeout(resolve, 100)); 
-            let nowDate = new Date().getTime();
-            
-            let valuesHash = {};
-            for (let keyPkg of atts) valuesHash[keyPkg.key] = keyPkg.value;
-            valuesHash.ts_id = nowDate;
-            let telemetryHashArray = [ {key:'ts',value:nowDate} , {key:'values',value:valuesHash} ];
-            
-            saveTelemetry(telemetryHashArray)
-        }
-    );
-
-
-// Update esa mondá
-function saveTelemetry(telemetryHashArray){
-    attributeService.saveEntityTimeseries(entityId, 'ANY', telemetryHashArray)
-                    .subscribe(function () {widgetContext.updateAliases();})
-}
+    async function(atts){
+        await new Promise(resolve => setTimeout(resolve, 1000)); 
+        let nowDate = new Date().getTime();
+        
+        let valuesHash = {};
+        for (let keyPkg of atts) valuesHash[keyPkg.key] = keyPkg.value;
+        valuesHash.ts_id = nowDate;
+        let telemetryHashArray = [ {key:'ts',value:nowDate}, {key:'values',value:valuesHash} ];
+        
+        // save esa mondá and wait 2 seconds to update dashboard
+        attributeService.saveEntityTimeseries(entityId, 'ANY', telemetryHashArray)
+                        .subscribe(function () { ()=>{setTimeout(widgetContext.updateAliases(),2000) };})
+    }
+);
