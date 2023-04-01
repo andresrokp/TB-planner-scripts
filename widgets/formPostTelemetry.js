4768d2740f74c2b3bcc10041b137598c327f138f
andresropk <afrokap@gmail.com>
2023-04-01 14:08:20 -0500

    fix: include airline and name in the telemetry

--- widgets/formPostTelemetry.js
+++ widgets/formPostTelemetry.js
@@ -6,7 +6,7 @@ let $injector = widgetContext.$scope.$injector;
 let attributeService = $injector.get(widgetContext.servicesMap.get('attributeService'));
 
 // keys to fetch
-let keys = ['regNum','sta','std'];
+let keys = ['airline','regNum','sta','std'];
 
 // get SHARED attributes -> 
 attributeService.getEntityAttributes(entityId, 'SHARED_SCOPE', keys).subscribe(
@@ -17,9 +17,10 @@ attributeService.getEntityAttributes(entityId, 'SHARED_SCOPE', keys).subscribe(
         let valuesHash = {};
         for (let keyPkg of atts) valuesHash[keyPkg.key] = keyPkg.value;
         valuesHash.ts_id = nowDate;
+        valuesHash.fltNum = entityName;
         let telemetryHashArray = [ {key:'ts',value:nowDate}, {key:'values',value:valuesHash} ];
         
-        // save esa mondá and wait 2 seconds to update dashboard
+        // save esa mondá and wait 2 secondss to update dashboard
         attributeService.saveEntityTimeseries(entityId, 'ANY', telemetryHashArray)
                         .subscribe(function () { ()=>{setTimeout(widgetContext.updateAliases(),2000) };})
     }
