be627743aacea6a51cdb12eb99339435c80f3a41
andresropk <afrokap@gmail.com>
2023-04-18 19:39:11 -0500

    feat: so-so lat and long generator inside airport

--- others/randomPostDevice.js
+++ others/randomPostDevice.js
@@ -46,6 +46,83 @@ function getRandomDate(start, end) {
   ).toISOString();
 }
 
+/* This are the 4 points of interception of 4 lines that enclose an area
+
+[points]
+
+The lines enclose an area.
+
+Please elaborate a javascript function that return a (x,y) point values inside within the area enclosed by the 4 lines
+*/
+
+
+function getRandomPointInsideArea(point1, point2, point3, point4) {
+  // Calculate the minimum and maximum x and y values of the bounding box
+  // x is loggitude and y is latitude
+  const minX = Math.min(point1[0], point2[0], point3[0], point4[0]);
+  const maxX = Math.max(point1[0], point2[0], point3[0], point4[0]);
+  const minY = Math.min(point1[1], point2[1], point3[1], point4[1]);
+  const maxY = Math.max(point1[1], point2[1], point3[1], point4[1]);
+
+  // Generate a random point inside the bounding box
+  let x, y;
+  do {
+    x = Math.random() * (maxX - minX) + minX;
+    y = Math.random() * (maxY - minY) + minY;
+  } while (!isPointInsideArea([x, y], point1, point2, point3, point4));
+
+  return [x, y];
+}
+
+function isPointInsideArea(point, point1, point2, point3, point4) {
+  // Use the ray casting algorithm to determine if the point is inside the area
+  const intersectCount = countIntersections(point, point1, point2, point3, point4);
+  return intersectCount % 2 !== 0;
+}
+
+function countIntersections(point, point1, point2, point3, point4) {
+  // Calculate the slope and y-intercept of each line segment
+  const slope1 = getSlope(point1, point2);
+  const yIntercept1 = getYIntercept(point1, slope1);
+  const slope2 = getSlope(point2, point3);
+  const yIntercept2 = getYIntercept(point2, slope2);
+  const slope3 = getSlope(point3, point4);
+  const yIntercept3 = getYIntercept(point3, slope3);
+  const slope4 = getSlope(point4, point1);
+  const yIntercept4 = getYIntercept(point4, slope4);
+
+  // Check how many times a ray from the point intersects the line segments
+  let intersectCount = 0;
+  if (point[1] > calculateY(point[0], slope1, yIntercept1)) {
+    intersectCount++;
+  }
+  if (point[1] > calculateY(point[0], slope2, yIntercept2)) {
+    intersectCount++;
+  }
+  if (point[1] < calculateY(point[0], slope3, yIntercept3)) {
+    intersectCount++;
+  }
+  if (point[1] < calculateY(point[0], slope4, yIntercept4)) {
+    intersectCount++;
+  }
+
+  return intersectCount;
+}
+
+function getSlope(point1, point2) {
+  return (point2[1] - point1[1]) / (point2[0] - point1[0]);
+}
+
+function getYIntercept(point, slope) {
+  return point[1] - slope * point[0];
+}
+
+function calculateY(x, slope, yIntercept) {
+  return slope * x + yIntercept;
+}
+
+
+
 // LLM chatgpt: Replace the corresponent function (getRandomInt, getRandomInt, getRandomBoolean, getRandomString, getRandomToken, getRandomDate) in front of each key inside the Original Big JSON. This is the original big JSON:
 
 let msgBody = {
@@ -136,7 +213,6 @@ let msgBody = {
 };
 
 let {credentials} = require('../myvars')
-const fetch = require('node-fetch');
 
 async function sendMessageToDevice(deviceId, message) {
   const url = `https://${credentials.dns}/api/v1/${deviceId}/telemetry`;
@@ -154,4 +230,9 @@ async function sendMessageToDevice(deviceId, message) {
   }
 }
 
-sendMessageToDevice()
+let point1 = [21.038764, -86.875932]
+let point2 = [21.033697, -86.868137]
+let point3 = [21.034798, -86.867333]
+let point4 = [21.039685, -86.875372]
+
+console.log(getRandomPointInsideArea(point1, point2, point3, point4))
