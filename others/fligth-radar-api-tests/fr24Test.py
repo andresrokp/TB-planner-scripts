2f7afc6b6591db6c1b5251b58d718431aa44f238
andresropk <afrokap@gmail.com>
2023-06-01 09:00:38 -0500

    feat: OK fetching and printing data from fr24

--- others/fligth-radar-api-tests/fr24Test.py
+++ others/fligth-radar-api-tests/fr24Test.py
@@ -1,8 +1,28 @@
 import os
 from dotenv import load_dotenv, dotenv_values
+from FlightRadar24.api import FlightRadar24API
+from FlightRadar24.flight import Flight
+from pprint import pprint
 
 load_dotenv()
-
 print("hello python fr24 test")
-print(os.getenv("TEST_VAR"))
 
+
+fr_api = FlightRadar24API()
+
+def test_get_flight_details():
+    flight = fr_api.get_flights()[-1]
+    details = fr_api.get_flight_details(flight.id)
+    return details
+
+flightDetails = test_get_flight_details()
+
+
+print('\n- - - details\n',flightDetails.keys())
+# dict_keys(['identification', 'status', 'level', 'promote', 'aircraft', 'airline', 
+# 'owner', 'airspace', 'airport', 'flightHistory', 'ems', 'availability', 'time', 
+# 'trail', 'firstTimestamp', 's'])
+
+print('\n- - - status\n',flightDetails["status"])
+print('\n- - - airport\n')
+pprint(flightDetails["airport"])
