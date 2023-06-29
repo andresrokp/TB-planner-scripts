979dc3b8df8e8c7c4487c99f74da2efa9e26281c
andresropk <afrokap@gmail.com>
2023-06-29 16:37:13 -0500

    feat: f() to calculate fuel % with min and max

--- widgets/filtrosConversionesTablas.js
+++ widgets/filtrosConversionesTablas.js
@@ -3,3 +3,7 @@ function fuelFilterPostFunctionAvoidPikes(){
     if (value > 10000 || value < 10 ) return prevValue;
     return value;
 }
+
+function fuelPercentageAdvanceCellContent(){
+    return (value-entity.min_fuel)/(entity.max_fuel-entity.min_fuel) * 100;
+}
