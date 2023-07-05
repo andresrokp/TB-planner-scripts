bd637ebeb25594865946e884f8ba110ed37eb523
andresropk <afrokap@gmail.com>
2023-07-05 11:21:55 -0500

    refactor: script order, include row arguments

--- others/auto-POSTS/fromCsvFile.js
+++ others/auto-POSTS/fromCsvFile.js
@@ -10,6 +10,7 @@ function getColumnsName(csvFilePath) {
   return new Promise((res,rej)=>{
     let stream = fs.createReadStream(csvFilePath, {encoding:'utf8'});
     let columnsName;
+    
     stream.on('data', (chunk)=>{
         columnsName = chunk.split(/\r\n|\n/)[0].split(';');
         stream.close();
@@ -21,75 +22,63 @@ function getColumnsName(csvFilePath) {
 }
 
 function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, 2000));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function getRandomMillis() {
   return Math.floor(Math.random() * (1000 - 500 + 1) + 500);
 }
 
-async function processCsvBody(csvFilePath, columnsName) {
+async function processCsvBody(csvFilePath, columnsName, startRow, endRow) {
   const parser = fs.createReadStream(csvFilePath)
   .pipe(csv_parser({
       delimiter: ';',
-      columns: columnsName,//['Timestamp','axisX','axisY','axisZ','battery','course','driverUniqueId','emergency','event','fuel','geofenceIds','hdop','horometerAlt','ignition','io24','io248','io483','io68','lastUpdate','latitude','longitude','motion','odometer','outdated','pdop','power','proximity','sat','speed','start','temp1','tripOdometer','valid'],
-      from_line: 6,
-      to_line: 10
+      columns: columnsName, //['Timestamp','axisX','axisY','axisZ','battery','course','driverUniqueId','emergency','event','fuel','geofenceIds','hdop','horometerAlt','ignition','io24','io248','io483','io68','lastUpdate','latitude','longitude','motion','odometer','outdated','pdop','power','proximity','sat','speed','start','temp1','tripOdometer','valid'],
+      from_line: startRow,
+      to_line: endRow
     })
   );
-
-  //   console.log(parser);
 
   let previousTimestamp = null;
   let isFirstRow = true;
 
   for await (const telemetryRow of parser) {
-    const timestamp = isFirstRow ? Date.now() : Date.parse(telemetryRow['timestamp']);
-    const delayTime = isFirstRow ? 0 : timestamp - previousTimestamp + getRandomMillis();
-    previousTimestamp = timestamp;
-    isFirstRow = false;
+    
+    // // dynamic wait time between operation
+    // const timestamp = isFirstRow ? Date.now() : Date.parse(telemetryRow['timestamp']);
+    // const delayTime = isFirstRow ? 0 : timestamp - previousTimestamp + getRandomMillis();
+    // previousTimestamp = timestamp;
+    // isFirstRow = false;
 
+    delayTime = 5000; // static waiting value... remove
     await delay(delayTime);
 
-    const deviceId = telemetryRow['device_id'];
-    delete telemetryRow['timestamp'];
-    delete telemetryRow['device_id'];
-
-    const telemetryData = {
-      [timestamp]: telemetryRow,
-    };
-
-    await postTelemetry(deviceId, telemetryData);
+    await postTelemetry(telemetryRow);
   }
 }
 
 
-async function postTelemetry(deviceId, telemetryData) {
-    console.log(telemetryData);
-//   const url = `https://${credentials.dns}/api/v1/${deviceId}/telemetry`;
-//   const response = await fetch(url, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(telemetryData),
-//   });
+async function postTelemetry(telemetryData) {
+  console.log(telemetryData);
+  const url = `https://${process.env.TB_DNS}/api/v1/${process.env.CUN_TEST_TOKEN}/telemetry`;
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(telemetryData),
+  });
   
-//   if (response.ok) {
-//     console.log('Telemetry posted successfully');
-//   } else {
-//     console.log('Failed to post telemetry:', response.status);
-//   }
+  if (response.ok) { console.log('Telemetry posted successfully'); }
+  else { console.log('Failed to post telemetry:', response.status); }
 }
+
 
 
 //-----------------------------------------------------------------
 // main execution
 
 const csvFilePath = process.env.CSV_FILE_PATH;
-const credentials = {
-  dns: 'your-thingsboard-dns',
-};
+let startRow = 20;
+let endRow = 25;
 
 
 (async ()=>{
@@ -97,17 +86,8 @@ const credentials = {
   
   console.log(columnsName)
   
-  processCsvBody(csvFilePath, columnsName).catch((err) => {
+  processCsvBody(csvFilePath, columnsName, startRow, endRow).catch((err) => {
     console.error('Error processing CSV:', err);
   });
 })();
-
-
-// sendTelemetryData(csvFilePath, deviceToken, startRow, endRow)
-//   .then(() => {
-//     console.log('Telemetry data sent successfully.');
-//   })
-//   .catch((error) => {
-//     console.error('Error sending telemetry data:', error);
-//   });
 
