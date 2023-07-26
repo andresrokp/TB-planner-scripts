83aaa79876c5310f52501a392e134fe1a1ce0e53
andresrokp <afrokap@gmail.com>
2023-07-25 19:31:51 -0500

    chore: set for stress test

--- others/auto-POSTS/randomPostDevice.js
+++ others/auto-POSTS/randomPostDevice.js
@@ -1,5 +1,5 @@
 const log = console.log;
-
+var counter = 0
 
 let {credentials, testDevices, jueputacalletano} = require('../../myvars')
 let {buildMsg} = require('./auxRPD')
@@ -15,9 +15,9 @@ async function sendMessageToDevice(deviceId, message) {
   // try the post
   try {
     const response = await fetch(url, { method: 'POST', headers, body });
-    log(`Message sent to device ${message.device.uniqueId} - ${message.device.name}`); // response
+    log(`${counter++} :: Message sent to device ${message.device.uniqueId} - ${message.device.name}`); // response
   } catch (error) {
-    console.error(`Error sending message to device ${deviceId}:`, error);
+    console.error(`${counter++} :: Error sending message to device ${deviceId}:`);
   }
 }
 
@@ -35,25 +35,25 @@ function mainALoCerdo(qty){
   }
 }
 
-function mainByStep(qty, t){
-  log('post with delays')
-  let i = 0
-  const timer = setInterval(() => {
-    let device = testDevices[i]
-    log(i,device)
-    let msgBody = buildMsg(device);
-    // sendMessageToDevice(jueputacalletano,msgBody);
-    if(i == qty || i == testDevices.length){
-      clearInterval(timer);
-    }
-    i++
-  }, t);
-}
+// function mainByStep(qty, t){
+//   log('post with delays')
+//   let i = 0
+//   const timer = setInterval(() => {
+//     let device = testDevices[i]
+//     log(i,device)
+//     let msgBody = buildMsg(device);
+//     // sendMessageToDevice(jueputacalletano,msgBody);
+//     if(i == qty || i == testDevices.length){
+//       clearInterval(timer);
+//     }
+//     i++
+//   }, t);
+// }
 
 let start = performance.now()
 
 // to launch load test
-// Array(10).fill().forEach(()=>mainALoCerdo()) //
+Array(100).fill().forEach(()=>mainALoCerdo()) //
 
 // send without delays
 // mainALoCerdo()
