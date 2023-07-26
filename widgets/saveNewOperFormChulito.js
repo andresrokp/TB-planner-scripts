450cfebc9de9b373c571df66ccf6e7f0f32692b6
andresrokp <afrokap@gmail.com>
2023-07-25 20:04:16 -0500

    refactor: comment code

--- widgets/saveNewOperFormChulito.js
+++ widgets/saveNewOperFormChulito.js
@@ -5,23 +5,25 @@ if (!choose) return;
 let $injector = widgetContext.$scope.$injector;
 let attributeService = $injector.get(widgetContext.servicesMap.get('attributeService'));
 
-// get all widget's key names
+// keys to fetch
 let keys = widgetContext.datasources[0].dataKeys.map(dk => dk.name);
 
 // get SHARED attributes -> 
-
-attributeService.getEntityAttributes(entityId, 'SERVER_SCOPE', keys).subscribe(
+attributeService.getEntityAttributes(entityId, 'SHARED_SCOPE', keys).subscribe(
     async function(atts){
         await new Promise(resolve => setTimeout(resolve, 1000)); 
         let nowDate = new Date().getTime();
         
+        // join up data pairs Object from loaded atts
         let valuesHash = {};
         for (let keyPkg of atts) valuesHash[keyPkg.key] = keyPkg.value;
         valuesHash.ts_id = nowDate;
         valuesHash.fltNum = entityName;
+        
+        // adapt to TB expected data structure; 2 properties: a global ts and a sub-oject with all the data pairs
         let telemetryHashArray = [ {key:'ts',value:nowDate}, {key:'values',value:valuesHash} ];
         
-        // save esa mondá and wait 2 secondss to update dashboard
+        // save esa mondá and wait 2 seconds to update dashboard. TB update the teltry because the ts already existed before
         attributeService.saveEntityTimeseries(entityId, 'ANY', telemetryHashArray)
                         .subscribe(function () { ()=>{setTimeout(widgetContext.updateAliases(),2000) };})
     }
