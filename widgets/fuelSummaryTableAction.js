c71d1dcc70910d6ef29d2c8392d495ed146a193f
andresrokp <afrokap@gmail.com>
2023-07-27 13:21:42 -0500

    feat: get inferiors teltry point + refactor

--- widgets/fuelSummaryTableAction.js
+++ widgets/fuelSummaryTableAction.js
@@ -1,40 +1,54 @@
-console.log('widgetContext',widgetContext)
-console.log('startTimeMs', new Date(widgetContext.dashboard.dashboardTimewindow.history.fixedTimewindow.startTimeMs))
-console.log('endTimeMs',new Date(widgetContext.dashboard.dashboardTimewindow.history.fixedTimewindow.endTimeMs))
+console.log('widgetContext',widgetContext);
+
+let startTs = widgetContext.dashboard.dashboardTimewindow.history.fixedTimewindow.startTimeMs;
+let endTs = widgetContext.dashboard.dashboardTimewindow.history.fixedTimewindow.endTimeMs;
+let limit = 10;
+let orderBy = 'DESC'
+console.log('startTimeMs', new Date(startTs));
+console.log('endTimeMs',new Date(endTs));
 
 
 let $injector = widgetContext.$scope.$injector;
 let attributeService = $injector.get(widgetContext.servicesMap.get('attributeService'));
+let saveAtts = attributeService.saveEntityAttributes.bind(attributeService)
+let getTSeries = attributeService.getEntityTimeseries.bind(attributeService)
 
 let allEntitiesIds = widgetContext.datasources.map(ds => ({'entityType':ds.entityType,'id':ds.entityId}));
 let AllEntitiesName = widgetContext.datasources.map(ds => ds.entityName);
 
+let keys = ['fuel']
 
-// // get fuelConsumption values around TimeWindow interval extremes
-// attributeService.getEntityTimeseries(entityId,keys,ts_id-2,ts_id+2).subscribe(
-//         function(telemetry){
+// get fuelConsumption values around TimeWindow interval extremes
 
 // empacar la data in TB format
-let testData = [{key:'test-attribute',value:'hola-atributos-2'}];
+let testData = [{key:'test-attribute',value:'prueba-write-masivo'}];
 
-// write the values in device attribute
-async function setAllConsumos(){
-    for await (let id of allEntitiesIds){ //allEntitiesIds[0],allEntitiesIds[1]]){
-        console.log(id)
+getCalcSetEachDevice()
+
+
+/** function definitions **/
+
+// loop over entities and executes processes
+async function getCalcSetEachDevice(){
+    for await (let id of allEntitiesIds){//[allEntitiesIds[0],allEntitiesIds[1]]){ //
         //id should be an object {entityType, id}
-        const response = await saveEntityAttributesPromise(id,'SHARED_SCOPE',testData);
-      console.log(response);
+        await entityOperationsChainPromise(id);
     }
 }
 
 // a promise wrapper to enable await for each writing
-function saveEntityAttributesPromise(id, scope, data) {
-  return new Promise((resolve, reject) => {
-    attributeService.saveEntityAttributes(id, scope, data).subscribe(   (response) => resolve(response)  );
+function entityOperationsChainPromise(entityId) {
+  return new Promise((resolve,reject) => {
+    // get all inferiors
+    orderBy = 'DESC';
+    // console.log(entityId)
+    // console.log(keys, startTs, endTs, limit, orderBy)
+    getTSeries(entityId, keys, startTs, endTs, limit, orderBy).subscribe(
+        function(superiorValue){            
+            console.log(superiorValue)
+            resolve()
+            // saveAtts(id, 'SERVER_SCOPE', data).subscribe(   () => resolve()  );
+        })
+    
   });
 }
-
-setAllConsumos()
-
-// console.log('allEntitiesIds',allEntitiesIds)
-// console.log('AllEntitiesNames',AllEntitiesName)
