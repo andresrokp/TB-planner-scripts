e8a9d6fc6fa3e3d91fb2cef90414a09af45df278
andresrokp <afrokap@gmail.com>
2023-09-06 17:19:10 -0500

    feat: pretty print and fetch QR code

--- views/checklist360.js
+++ views/checklist360.js
@@ -1,72 +1,16 @@
-function saveChecklist360Teltry() {
-    console.log(widgetContext);
 
-    // Confirmation alert
-    let choose = confirm('¿Confirma el guardado de la información?');
-    if (!choose) return;
-    
-    let $injector = widgetContext.$scope.$injector;
-    let attributeService = $injector.get(widgetContext.servicesMap.get('attributeService'));
-    
-    // get all widget's key names
-    let keys = widgetContext.datasources[0].dataKeys.map(dk => dk.name);
-    console.log('-----------------\nkeys',keys)
-    
-    // get SHARED attributes
-    attributeService.getEntityAttributes(entityId, 'SHARED_SCOPE', keys).subscribe(
-        async function(atts){
-            
-            console.log('loaded atts',atts);
-            await new Promise(resolve => setTimeout(resolve, 1000)); 
-            
-            let valuesHash = {};
-            for (let keyPkg of atts){
-                // // si la key no es un Simple letrero y el value no existe
-                // if (!keyPkg.key.includes('-<') && !keyPkg.value){
-                //     alert("ERROR: Hay valores no definidos");
-                //     return;
-                // }
-                valuesHash[keyPkg.key] = keyPkg.value;
-            }
-            let nowDate = new Date().getTime();
-            valuesHash.ts_id = nowDate;
-            
-            let telemetryHashArray = [ {key:'ts',value:nowDate}, {key:'values',value:valuesHash} ];
-            console.log('telemetryHashArray',telemetryHashArray)
-            
-            // save esa mondá, clear form via att deletion, and wait 1 second to refresh
-            attributeService.saveEntityTimeseries(entityId, 'ANY', telemetryHashArray)
-            .subscribe( function(resp){
-                console.log('resp',resp);
-                // // ---------- Clear Form ----------
-                // // format keys list as obj array
-                // keys = keys.map(k => ({'key':k}));
-                // // delete and then refresh
-                // attributeService.deleteEntityAttributes(entityId, 'SHARED_SCOPE', keys)
-                // .subscribe(function () {
-                //     ()=>{setTimeout(widgetContext.updateAliases(),1000)};
-                // })
-            })
-        }
-    ); 
-}
+console.log('entityName',entityName)
+console.log('entityId',entityId)
+console.log('additionalParams',additionalParams)
+console.log('widgetContext',widgetContext)
+
+let infoPrint = JSON.stringify({entityName,entityId},null,4);
+let infoFetch = encodeURIComponent(JSON.stringify({entityName,entityId}));
 
 
-
-function rowActionButton_popEntityQRCode() {
-
-    console.log('entityName',entityName)
-    console.log('entityId',entityId)
-    console.log('additionalParams',additionalParams)
-    console.log('widgetContext',widgetContext)
-
-    let info = {entityName,entityId};
-
-    widgetContext.dialogs
-    .alert('Qr Generado',
-        `<br>Información:
-        <br><pre>${info}</pre>
-        <br>Imágen:
-        <br><img style="width:50px;" src="https://www.sighums.com/wp-content/uploads/2023/03/logo2-1.png">`).subscribe();
-
-}
+widgetContext.dialogs
+.alert('Qr Generado',
+       `<br>Información:
+       <br><pre>${infoPrint}</pre>
+       <br>Imágen:
+       <br><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${infoFetch}">`).subscribe();
