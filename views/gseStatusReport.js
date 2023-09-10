4727a6db479db189eb7cad0c7a338b87075db718
andresrokp <afrokap@gmail.com>
2023-09-10 12:52:53 -0500

    feat: delta hours to next mntto dynamic Get

--- views/gseStatusReport.js
+++ views/gseStatusReport.js
@@ -7,9 +7,17 @@ function auxDash_simpleCard_settings_widgetCss(params) {
 }
 
 
-function auxDash_multAttsUpdate_dataProxMntto_getValue(params) {
+function auxDash_multAttsUpdate_dataAbsoluteProxMnttoA_setValue() {
     let hrsActuales = ctx.$scope.multipleInputWidget.multipleInputFormGroup.value['3'];
     let hrsToNext = ctx.$scope.multipleInputWidget.multipleInputFormGroup.value['4'];
     
-    return hrsActuales + hrsToNext;
+    return Math.floor(hrsActuales + hrsToNext);
+}
+
+
+function auxDash_multAttsUpdate_dataDeltaHrsToNextMntto_getValue() {
+    let hrsProxMntoA = ctx.data[4].data[0][1]
+    let hrsActuales = Math.floor(ctx.data[2].data[0][1]/3600)
+    
+    return hrsProxMntoA - hrsActuales;
 }
