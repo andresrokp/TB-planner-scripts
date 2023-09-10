2e750baf03a88ae58c2aca9e4705d3838b79cce8
andresrokp <afrokap@gmail.com>
2023-09-10 11:24:37 -0500

    feat: set next mntto absolute hours calc

--- views/gseStatusReport.js
+++ views/gseStatusReport.js
@@ -5,3 +5,11 @@ function auxDash_simpleCard_settings_widgetCss(params) {
         }
     }
 }
+
+
+function auxDash_multAttsUpdate_dataProxMntto_getValue(params) {
+    let hrsActuales = ctx.$scope.multipleInputWidget.multipleInputFormGroup.value['3'];
+    let hrsToNext = ctx.$scope.multipleInputWidget.multipleInputFormGroup.value['4'];
+    
+    return hrsActuales + hrsToNext;
+}
