b450470d869c521e78e6937f918bdb46eb0e11fe
andresrokp <afrokap@gmail.com>
2023-09-24 19:25:30 -0500

    refactor: clean up Dialog's ng code + comment out

--- views/gestionDeOperarios/iButtonNameCustomAction.js
+++ views/gestionDeOperarios/iButtonNameCustomAction.js
@@ -14,9 +14,6 @@ let deviceService = $injector.get(
 let attributeService = $injector.get(
   widgetContext.servicesMap.get("attributeService")
 );
-let entityRelationService = $injector.get(
-  widgetContext.servicesMap.get("entityRelationService")
-);
 
 openEditEntityDialog();
 
@@ -38,32 +35,23 @@ function EditEntityDialogController(instance) {
     to: "TO",
   };
   vm.attributes = {};
-  vm.oldRelationsData = [];
   vm.relationsToDelete = [];
   vm.entity = {};
-  // le meto una nueva cosa al modelo to have it available in the view. El additionalParams[1] es el iButton Tag... el asignado en vm debería llamarse iButtonValue o algo así mejor
-  vm.additionalParams = additionalParams[1];
+  // le meto una nueva cosa al modelo to have it available in the view. El additionalParams[1] del widget es el iButtonTag (en el orden actual).
+  vm.iButtonTag = additionalParams[1];
 
   // esto está altamente relacionado con la estructura de la Form. Los null son para limpiar los campos . 'fb' means 'FormBuilder' is a calling to the Angular's REACTIVE Form Module . 'vm.editEntityFormGroup' ends being the main variable bag of the things inside the form . Thats why its object definition has the same shape of the input fields 
   vm.editEntityFormGroup = vm.fb.group({
     entityName: ["", [vm.validators.required]],
-    entityType: [null],
-    entityLabel: [null],
-    type: ["", [vm.validators.required]],
     // it seems this shit can be nested!!
     attributes: vm.fb.group({
       // como se llame aquí, se debe llamar el mat-form-field>input>formControlName
-      pruebaEscritura: [null],
+    //   pruebaEscritura: [null],
       // Valor dinámico que le meto al objeto y debe corresponder con un matFormField embebido dinámico
-      [additionalParams[1]]: [null],
-      address: [null],
-      owner: [null],
-      number: [null, [vm.validators.pattern(/^-?[0-9]+$/)]],
-      booleanValue: [false],
+      [vm.iButtonTag]: [null]
     }),
     // also can be streamed!!
-    oldRelations: vm.fb.array([]),
-    relations: vm.fb.array([]),
+    // relations: vm.fb.array([]),
   });
 
   getEntityInfo();
@@ -76,65 +64,11 @@ function EditEntityDialogController(instance) {
     return vm.editEntityFormGroup.get("relations");
   };
 
-  vm.oldRelations = function () {
-    return vm.editEntityFormGroup.get("oldRelations");
-  };
-
-  vm.addRelation = function () {
-    vm.relations().push(
-      vm.fb.group({
-        relatedEntity: [null, [vm.validators.required]],
-        relationType: [null, [vm.validators.required]],
-        direction: [null, [vm.validators.required]],
-      })
-    );
-  };
-
-  function addOldRelation() {
-    vm.oldRelations().push(
-      vm.fb.group({
-        relatedEntity: [
-          {
-            value: null,
-            disabled: true,
-          },
-          [vm.validators.required],
-        ],
-        relationType: [
-          {
-            value: null,
-            disabled: true,
-          },
-          [vm.validators.required],
-        ],
-        direction: [
-          {
-            value: null,
-            disabled: true,
-          },
-          [vm.validators.required],
-        ],
-      })
-    );
-  }
-
-  vm.removeRelation = function (index) {
-    vm.relations().removeAt(index);
-    vm.relations().markAsDirty();
-  };
-
-  vm.removeOldRelation = function (index, relation) {
-    vm.oldRelations().removeAt(index);
-    vm.relationsToDelete.push(relation);
-    vm.oldRelations().markAsDirty();
-  };
-
   vm.save = function () {
     vm.editEntityFormGroup.markAsPristine();
     widgetContext.rxjs
       .forkJoin([
         saveAttributes(entityId),
-        saveRelations(entityId),
         saveEntity(),
       ])
       .subscribe(function () {
@@ -145,55 +79,30 @@ function EditEntityDialogController(instance) {
 
   // BUSINESS METHODS DEFINITION
 
+// This is not a pure get... This is a extractor/setter. Extract the atts data from the parameter, and set it as the values of the vm.attributes JSON
   function getEntityAttributes(attributes) {
     for (var i = 0; i < attributes.length; i++) {
       vm.attributes[attributes[i].key] = attributes[i].value;
     }
   }
 
-  function getEntityRelations(relations) {
-    let relationsFrom = relations[0];
-    let relationsTo = relations[1];
-    for (let i = 0; i < relationsFrom.length; i++) {
-      let relation = {
-        direction: "FROM",
-        relationType: relationsFrom[i].type,
-        relatedEntity: relationsFrom[i].to,
-      };
-      vm.oldRelationsData.push(relation);
-      addOldRelation();
-    }
-    for (let i = 0; i < relationsTo.length; i++) {
-      let relation = {
-        direction: "TO",
-        relationType: relationsTo[i].type,
-        relatedEntity: relationsTo[i].from,
-      };
-      vm.oldRelationsData.push(relation);
-      addOldRelation();
-    }
-  }
-
   function getEntityInfo() {
     widgetContext.rxjs
       .forkJoin([
-        entityRelationService.findInfoByFrom(entityId),
-        entityRelationService.findInfoByTo(entityId),
         attributeService.getEntityAttributes(entityId, "SERVER_SCOPE"),
         entityService.getEntity(entityId.entityType, entityId.id),
       ])
       .subscribe(function (data) {
-        getEntityRelations(data.slice(0, 2));
-        getEntityAttributes(data[2]);
-        vm.entity = data[3];
+        // getEntityRelations(data.slice(0, 2));
+        getEntityAttributes(data[0]);
+        vm.entity = data[1];
         vm.editEntityFormGroup.patchValue(
           {
             entityName: vm.entity.name,
-            entityType: vm.entityType,
-            entityLabel: vm.entity.label,
-            type: vm.entity.type,
-            attributes: vm.attributes,
-            oldRelations: vm.oldRelationsData,
+            // entityType: vm.entityType,
+            // entityLabel: vm.entity.label,
+            // type: vm.entity.type,
+            attributes: vm.attributes
           },
           {
             emitEvent: false,
@@ -227,66 +136,23 @@ function EditEntityDialogController(instance) {
       }
     }
     if (attributesArray.length > 0) {
-      // jala el customer y guarda copia en él
+      // jala el Device, toma el customerId y guarda copia en él
       deviceService.getDevice(entityId.id).subscribe((resp) => {
-        console.log("getDeviceResp", resp);
+        // console.log("getDeviceResp", resp);
         attributeService
           .saveEntityAttributes(
             resp.customerId,
             "SERVER_SCOPE",
             attributesArray
           )
-          .subscribe((resp2) => {
-            console.log("saveEntityAttributesResp", resp2);
-          });
+          .subscribe();
       });
+      // Normal thing. Save Device Atts
       return attributeService.saveEntityAttributes(
         entityId,
         "SERVER_SCOPE",
         attributesArray
       );
-    }
-    return widgetContext.rxjs.of([]);
-  }
-
-  function saveRelations(entityId) {
-    let relations = vm.editEntityFormGroup.get("relations").value;
-    let tasks = [];
-    for (let i = 0; i < relations.length; i++) {
-      let relation = {
-        type: relations[i].relationType,
-        typeGroup: "COMMON",
-      };
-      if (relations[i].direction == "FROM") {
-        relation.to = relations[i].relatedEntity;
-        relation.from = entityId;
-      } else {
-        relation.to = entityId;
-        relation.from = relations[i].relatedEntity;
-      }
-      tasks.push(entityRelationService.saveRelation(relation));
-    }
-    for (let i = 0; i < vm.relationsToDelete.length; i++) {
-      let relation = {
-        type: vm.relationsToDelete[i].relationType,
-      };
-      if (vm.relationsToDelete[i].direction == "FROM") {
-        relation.to = vm.relationsToDelete[i].relatedEntity;
-        relation.from = entityId;
-      } else {
-        relation.to = entityId;
-        relation.from = vm.relationsToDelete[i].relatedEntity;
-      }
-      tasks.push(
-        entityRelationService.deleteRelation(
-          relation.from,
-          relation.type,
-          relation.to
-        )
-      );
-    }
-    if (tasks.length > 0) {
-      return widgetContext.rxjs.forkJoin(tasks);
     }
     return widgetContext.rxjs.of([]);
   }
