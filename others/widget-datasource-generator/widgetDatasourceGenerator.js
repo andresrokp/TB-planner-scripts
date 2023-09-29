d571cffc607f500a6318919c20c7a76e200a37ba
andresrokp <afrokap@gmail.com>
2023-09-28 19:46:10 -0500

    feat: generates big array with datakey form

--- others/widget-datasource-generator/widgetDatasourceGenerator.js
+++ others/widget-datasource-generator/widgetDatasourceGenerator.js
@@ -13,9 +13,50 @@ function readInputArrayFromFile(filename) {
   }
 }
 
+// Function to convert a label/name object to the datasource form
+function convertToTbForm(dataKey) {
+    return {
+        "name": dataKey.name,
+        "type": "attribute",
+        "label": dataKey.label,
+        "color": "#2196f3",
+        "settings": {
+        "dataKeyHidden": false,
+        "dataKeyType": "server",
+        "dataKeyValueType": "select",
+        "required": true,
+        "isEditable": "editable",
+        "selectOptions": [
+            {"value": "1", "label": "CUMPLE"},
+            {"value": "-1", "label": "NO CUMPLE"},
+            {"value": "0", "label": "NO APLICA"}
+        ],
+        "useCustomIcon": false,
+        "useGetValueFunction": false,
+        "useSetValueFunction": false
+        },
+        "_hash": Math.random(),
+        "aggregationType": null,
+        "units": null,
+        "decimals": null,
+        "funcBody": null,
+        "usePostProcessing": null,
+        "postFuncBody": null
+    };
+}
+// --------------------------
+// --------- Inicio ejecución
 
-
-// Inicio ejecución
+// json reading
 const filename = 'others/widget-datasource-generator/widgetDatasource.json'; // Replace with the path to your JSON file
 const inputArray = readInputArrayFromFile(filename);
 console.log(inputArray);
+
+// array form conversion
+const arrayDatakeys = inputArray.map(convertToTbForm)
+const datakeysObj = {
+"dataKeys": arrayDatakeys
+};
+
+
+console.log(JSON.stringify(datakeysObj, null, 2));
