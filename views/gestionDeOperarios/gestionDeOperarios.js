359e4269c89e865b6359e25ea5a8fcbe43b93b12
andresrokp <afrokap@gmail.com>
2023-09-29 16:53:24 -0500

    chore: getting and login most of the data

--- views/gestionDeOperarios/gestionDeOperarios.js
+++ views/gestionDeOperarios/gestionDeOperarios.js
@@ -25,3 +25,60 @@ function mainTable_dataiButton2_cellContent(params) {
     // it is not possible to fetch something because this is a sync function that doesnt wait
     // TODO: checke if i can perform hard changes in the table content. And write the cells directly from the subscribe function    
 }
+
+
+function mainTable_cellAction_saveRowToCustomer(){
+  console.log('widgetContext',widgetContext);
+  const confirmacion = confirm('¿Confirmar guardar la información?');
+  if (!confirmacion) return;
+
+  console.log('additionalParams',additionalParams);
+
+
+  console.log("entityId", entityId);
+  console.log("additionalParams", additionalParams);
+  console.log("widgetContext", widgetContext);
+
+  let $injector = widgetContext.$scope.$injector;
+  let entityService = $injector.get(widgetContext.servicesMap.get("entityService"));
+  let deviceService = $injector.get(widgetContext.servicesMap.get("deviceService"));
+  let attributeService = $injector.get(widgetContext.servicesMap.get("attributeService"));
+
+  const compoundDataKeys = widgetContext.datasources[0].dataKeys
+  const dataKeys = compoundDataKeys.map(dk => dk.name);
+  console.log("dataKeys", dataKeys);
+
+  let attributesArray = [];
+
+  // traverse the array and build a named array
+  dataKeys.forEach( (dk,i) => {
+    attributesArray.push({
+        key: dk,
+        value: additionalParams[i+1], //addPar has 1 element more
+      });
+  })
+  console.log("attributesArray", attributesArray);
+          
+  // // jala el Device, toma el customerId y guarda copia en él
+  deviceService.getDevice(entityId.id).subscribe((resp) => {
+          console.log("customerId", resp.customerId);
+          
+          // attributeService
+          //   .saveEntityAttributes(
+          //     resp.customerId,
+          //     "SERVER_SCOPE",
+          //     attributesArray
+          //   )
+          //   .subscribe();
+        });
+  //       // Normal thing. Save Device Atts
+  //       attributeService.saveEntityAttributes(
+  //         entityId,
+  //         "SERVER_SCOPE",
+  //         attributesArray
+  //       );
+  //     }
+  //   }
+  // }
+
+}
