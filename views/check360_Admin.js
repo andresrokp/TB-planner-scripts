4a0a6d06c23887d6c181eb9c8f29788b3e64e681
andresrokp <afrokap@gmail.com>
2023-10-07 18:05:47 -0500

    feat: aux-history calc and show cumplimiento

--- views/check360_Admin.js
+++ views/check360_Admin.js
@@ -13,3 +13,24 @@ function auxDash_history_actionCell_showPhoto(params) {
                 widgetContext.dialogs.alert('Foto del registro',`<img width="600px" src="${img64}"/>`).subscribe();
             });
 }
+
+
+function auxDash_history_dataCumplimiento(params) {
+    // console.log('rowData',rowData);
+    // console.log('ctx',ctx);
+
+    let acum = 0;
+    let cosas = Object.keys(rowData).length - 5;
+
+    for (let dato in rowData){
+        // console.log('dato',dato);
+        // console.log('rowData.dato',rowData.dato);
+        if ( rowData[dato] == 1.0000001 || rowData[dato] == 1.0000002) {
+            acum++;
+        }
+    }
+
+    // console.log('acum',acum);
+    // console.log('cosas',cosas);
+    return Math.round(acum / cosas * 100);
+}
