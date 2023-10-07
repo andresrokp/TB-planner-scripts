1850824084ede10f4541fd2a25796b7158cd0d3c
andresrokp <afrokap@gmail.com>
2023-10-06 20:54:02 -0500

    feat: showPhoto apart with cell button in history

--- views/checklist360.js
+++ views/checklist360.js
@@ -402,12 +402,14 @@ function auxDash_history_actionCell_showPhoto(params) {
     console.log('additionalParams',additionalParams);
     console.log('widgetContext',widgetContext);
 
-    const attsServ = widgetContext.$scope.$injector.get(widgetContext.servicesMap.get('attributeService'))
+    const attsServ = widgetContext.$scope.$injector.get(widgetContext.servicesMap.get('attributeService'));
 
     const ts = additionalParams[0];
 
     attsServ.getEntityTimeseries(entityId,['fotografia','comentarios'],ts-2,ts+2)
-            .subscribe(function(telemetry){
-                console.log(telemetry)
-            })
+            .subscribe(function(teltry){
+                console.log(teltry);
+                const img64 = teltry.fotografia[0].value;
+                widgetContext.dialogs.alert('Foto del registro',`<img width="600px" src="${img64}"/>`).subscribe();
+            });
 }
