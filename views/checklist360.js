5012f58f42715e7580171214f18bdabae2775e4a
andresrokp <afrokap@gmail.com>
2023-10-06 20:57:34 -0500

    refactor: separate 360 oper de Admin

--- views/checklist360.js
+++ views/checklist360.js
@@ -396,20 +396,3 @@ function auxDash_checkInputForm () {
         
     }
 }
-
-
-function auxDash_history_actionCell_showPhoto(params) {
-    console.log('additionalParams',additionalParams);
-    console.log('widgetContext',widgetContext);
-
-    const attsServ = widgetContext.$scope.$injector.get(widgetContext.servicesMap.get('attributeService'));
-
-    const ts = additionalParams[0];
-
-    attsServ.getEntityTimeseries(entityId,['fotografia','comentarios'],ts-2,ts+2)
-            .subscribe(function(teltry){
-                console.log(teltry);
-                const img64 = teltry.fotografia[0].value;
-                widgetContext.dialogs.alert('Foto del registro',`<img width="600px" src="${img64}"/>`).subscribe();
-            });
-}
