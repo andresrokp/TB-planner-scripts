f73e4217558f53bdde02d24136cf7f8d0fbe701f
andresrokp <afrokap@gmail.com>
2024-02-26 16:16:54 -0500

    chore: msg trnsf include course property

--- rules/traccar-menzies.js
+++ rules/traccar-menzies.js
@@ -19,3 +19,65 @@ function esBuenMensaje_filter(params) {
 function beaconNonZeroLatLon_filter(params) {
     return msg.position.latitude != 0 && msg.position.longitude != 0;
 }
+
+function msgTransformador_script(params) {
+    // Extract data
+    var device = msg.device;
+    var position = msg.position;
+    var attributes = position.attributes;
+
+    // Construct new plain msg
+    var newMsg = {
+        "ts": new Date(position.fixTime).valueOf(),
+        "values": {
+            //msg.position.attributes
+            "sat": attributes.sat,
+            "event": attributes.event,
+            "ignition": attributes.ignition,
+            "motion": attributes.motion,
+            "emergency": attributes.di1,
+            "buzzer": attributes.out1,
+            "proximity": attributes.di2,
+            "pdop": attributes.pdop, // general presicion
+            "hdop": attributes.hdop, // horizontal presicion
+            "power": attributes.power,
+            "battery": attributes.battery,
+            "io68": attributes.io68,
+            "axisX": attributes.axisX,
+            "axisY": attributes.axisY,
+            "axisZ": attributes.axisZ,
+            "fuel": attributes.io327,
+            "tripOdometer": attributes.tripOdometer,
+            "odometer": attributes.odometer,
+            "temp1": attributes.temp1,
+            "io483": attributes.io483,
+            "horometerAlt": attributes.io449,
+            "rssi": attributes.rssi,
+            "io200": attributes.io200,
+            "io248": attributes.io248,
+            "adc1": attributes.adc1,
+            "io24": attributes.io24,
+            "driverUniqueId": attributes.driverUniqueId,
+            "beacon1Uuid": attributes.beacon1Uuid,
+            // msg.position
+            "outdated": position.outdated,
+            "valid": position.valid,
+            "latitude": position.latitude,
+            "longitude": position.longitude,
+            "speed": position.speed,
+            "course": position.course,
+            "accuracy": position.accuracy,
+            //msg.device
+            "lastUpdate": new Date(device.lastUpdate).valueOf(),
+            "geofenceIds": device.geofenceIds,
+            "devicename": device.name
+        },
+    };
+
+    // Return transformed message
+    return {
+        msg: newMsg,
+        metadata: { token: device.uniqueId },
+        msgType: msgType,
+    };
+}
